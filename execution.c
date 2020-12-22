4a0ef6e49382aa44b85c44d860692b6b47cde05c
gregoiremrr <greg.mourre@hotmail.fr>
2020-12-22 02:39:43 +0100

    declarFonctions

--- execution.c
+++ execution.c
@@ -52,3 +52,63 @@ void fonction(instructionHexa instruct, short* pile, int* ppc){
         case 99: halt(instruct, pile);
   }
 }
+
+void pop(instructionHexa instruct, short* pile){
+
+}
+
+void push(instructionHexa instruct, short* pile){
+
+}
+
+void iPop(instructionHexa instruct, short* pile){
+
+}
+
+void iPush(instructionHexa instruct, short* pile){
+
+}
+
+void push2(instructionHexa instruct, short* pile){
+
+}
+
+void call(instructionHexa instruct, short* pile, int* ppc){
+
+}
+
+void ret(instructionHexa instruct, short* pile, int* ppc){
+
+}
+
+void jmp(instructionHexa instruct, short* pile, int* ppc){
+
+}
+
+void jpc(instructionHexa instruct, short* pile, int* ppc){
+
+}
+
+void write(instructionHexa instruct, short* pile){
+
+}
+
+void read(instructionHexa instruct, short* pile){
+
+}
+
+void rnd(instructionHexa instruct, short* pile){
+
+}
+
+void dup(instructionHexa instruct, short* pile){
+
+}
+
+void op(instructionHexa instruct, short* pile){
+
+}
+
+void halt(instructionHexa instruct, short* pile){
+
+}
--- execution.h
+++ execution.h
@@ -14,4 +14,34 @@ void ReadLowLangageFile(FILE* fic, char** Prog);
 
 void fonction(instructionHexa instruct, short* pile, int* ppc);
 
+void pop(instructionHexa instruct, short* pile);
+
+void push(instructionHexa instruct, short* pile);
+
+void iPop(instructionHexa instruct, short* pile);
+
+void iPush(instructionHexa instruct, short* pile);
+
+void push2(instructionHexa instruct, short* pile);
+
+void call(instructionHexa instruct, short* pile, int* ppc);
+
+void ret(instructionHexa instruct, short* pile, int* ppc);
+
+void jmp(instructionHexa instruct, short* pile, int* ppc);
+
+void jpc(instructionHexa instruct, short* pile, int* ppc);
+
+void write(instructionHexa instruct, short* pile);
+
+void read(instructionHexa instruct, short* pile);
+
+void rnd(instructionHexa instruct, short* pile);
+
+void dup(instructionHexa instruct, short* pile);
+
+void op(instructionHexa instruct, short* pile);
+
+void halt(instructionHexa instruct, short* pile);
+
 #endif
